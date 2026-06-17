532762cf743065b305d263588f29c18cc1e93a61
ravijabade12 <ravisjabade12@gmail.com>
2026-06-17 13:16:35 +0530

    Add data object and GitHub endpoint to serve project information

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+const data = {
+  company: "TechCorp",
+  employeeCount: 250,
+  remoteWork: true,
+  headquarters: {
+    city: "San Francisco",
+    country: "USA",
+  },
+  departments: ["Engineering", "Marketing", "Sales"],
+  activeProjects: [
+    {
+      projectId: "PROJ-101",
+      name: "Cloud Migration",
+      budget: 50000.0,
+    },
+    {
+      projectId: "PROJ-102",
+      name: "AI Integration",
+      budget: 75000.0,
+    },
+  ],
+  ceo: null,
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -12,6 +36,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/ravi", (req, res) => {
   res.send("<h1>hello ravi</h1>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(data);
 });
 
 app.listen(process.env.PORT, () => {
